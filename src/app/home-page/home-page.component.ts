21fa9199e3bf3b564ae1ea5f5b812af576177cde
Unknown <kirby94@hotmail.it>
2018-06-22 22:45:18 +0200

    - Adapt drawing detail modal content to clicked drawing

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,37 +9,57 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 export class HomePageComponent implements OnInit {
 
-  closeResult: string;
   drawings = [
+
     {
       "file" : "DarthVader.jpg",
+      "title" : "Darth Cupid",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     },
+
     {
       "file" : "Jack.jpg",
+      "title" : "My name is Jack",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     },
+
     {
       "file" : "PacificRim.jpg",
+      "title" : "Red Pacifica",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     },
+
     {
       "file" : "Pinocchio.jpg",
+      "title" : "In the whale",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     },
+
     {
       "file" : "Snoopy.jpg",
+      "title" : "Chewpy",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     },
+
     {
       "file" : "Spyro.jpg",
+      "title" : "Purple Dragon",
+      "tags" : ["#tag1", "#tag2", "#tag3"]
     }
-    
+
   ];
+
+  selectedDrawing;
 
   constructor(private modalService: NgbModal) {}
 
   ngOnInit() {
   }
 
-  
-  open(content) {
+  openModal(content, drawing) {
+    this.selectedDrawing = drawing;
+    console.log(this.selectedDrawing);
     this.modalService.open(content);
   }
-  
+
 }
