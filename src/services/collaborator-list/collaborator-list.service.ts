7667e9f2a6c6f700b88dcc7ae4444b1dcf3feb4a
Unknown <kirby94@hotmail.it>
2018-06-24 11:24:29 +0200

    - Complete Collaborator-List service - "About" Page: update "About" section

--- src/services/collaborator-list/collaborator-list.service.ts
+++ src/services/collaborator-list/collaborator-list.service.ts
@@ -9,20 +9,92 @@ import { Names } from "../../interfaces/Names";
 export class CollaboratorListService {
 
   static names: Names = {
+    "Qwertee" : "Qwertee",
     "TeeTee" : "TeeTee",
-    "TeeFury" : "TeeFury"
+    "Pampling" : "Pampling",
+    "Wistitee" : "Wistitee",
+    "Othertees" : "Othertees",
+    "TeeFury" : "TeeFury",
+    "ShirtPunch" : "ShirtPunch",
+    "RIPTapparel" : "RIPTapparel",
+    "TheYeTee" : "TheYeTee",
+    "TeeVillain" : "TeeVillain",
+    "TeeTribe" : "TeeTribe",
+    "TeeCraze" : "TeeCraze",
+    "RedBubble" : "RedBubble",
+    "Loopslike" : "Loopslike"
   };
   
   list: Collaborator[] = [
     {
+      "name" : CollaboratorListService.names.Qwertee,
+      "website" : "https://www.qwertee.com/",
+      "logoFile" : CollaboratorListService.names.Qwertee + ".png"
+    },
+    {
       "name" : CollaboratorListService.names.TeeTee,
       "website" : "https://www.teetee.eu/en/",
-      "logoFile" : "TeeTee.png"
+      "logoFile" : CollaboratorListService.names.TeeTee + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.Pampling,
+      "website" : "https://www.pampling.com",
+      "logoFile" : CollaboratorListService.names.Pampling + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.Wistitee,
+      "website" : "https://www.wistitee.com/en/",
+      "logoFile" : CollaboratorListService.names.Wistitee + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.Othertees,
+      "website" : "https://www.othertees.com/",
+      "logoFile" : CollaboratorListService.names.Othertees + ".png"
     },
     {
       "name" : CollaboratorListService.names.TeeFury,
       "website" : "https://teefury.com",
-      "logoFile" : "TeeFury2.png"
+      "logoFile" : CollaboratorListService.names.TeeFury + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.ShirtPunch,
+      "website" : "https://www.shirtpunch.com",
+      "logoFile" : CollaboratorListService.names.ShirtPunch + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.RIPTapparel,
+      "website" : "",
+      "logoFile" : CollaboratorListService.names.RIPTapparel + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.TheYeTee,
+      "website" : "https://theyetee.com/",
+      "logoFile" : CollaboratorListService.names.TheYeTee + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.TeeVillain,
+      "website" : "https://www.teevillain.com/",
+      "logoFile" : CollaboratorListService.names.TeeVillain + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.TeeTribe,
+      "website" : "https://teetribe.eu",
+      "logoFile" : CollaboratorListService.names.TeeTribe + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.TeeCraze,
+      "website" : "https://teecraze.com/",
+      "logoFile" : CollaboratorListService.names.TeeCraze + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.RedBubble,
+      "website" : "https://www.redbubble.com",
+      "logoFile" : CollaboratorListService.names.RedBubble + ".png"
+    },
+    {
+      "name" : CollaboratorListService.names.Loopslike,
+      "website" : "https://www.loopslike.com/en",
+      "logoFile" : CollaboratorListService.names.Loopslike + ".png"
     }
   ];
 
