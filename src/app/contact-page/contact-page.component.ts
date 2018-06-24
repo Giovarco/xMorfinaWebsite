bf6087b33e8e7116d908cb51e73076b30303db82
Unknown <kirby94@hotmail.it>
2018-06-24 10:17:52 +0200

    - Create TabOpener Service - Open the website relative to the tee icon

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FacebookPageOpenerService } from "../../services/facebook-page-opener/facebook-page-opener.service";
 import { CollaboratorListService } from "../../services/collaborator-list/collaborator-list.service";
+import { TabOpenerService } from "../../services/tab-opener/tab-opener.service";
+import { Names } from '../../interfaces/Names';
+import { Collaborator } from '../../interfaces/Collaborator';
+import { environment } from "../../environments/environment";
 
 @Component({
   selector: 'app-contact-page',
@@ -9,11 +13,14 @@ import { CollaboratorListService } from "../../services/collaborator-list/collab
 })
 export class ContactPageComponent implements OnInit {
 
-  collaboratorNames;
-  collaboratorList;
-  
+  collaboratorNames: Names;
+  collaboratorList: Collaborator[];
+
+  facebookPageURL = environment.facebookPageURL;
+
   constructor(private facebookPageOpenerService: FacebookPageOpenerService,
-              private collaboratorListService: CollaboratorListService) { }
+              private collaboratorListService: CollaboratorListService,
+              private tabOpenerService: TabOpenerService) { }
 
   ngOnInit() {
     this.collaboratorNames = this.collaboratorListService.getNames();
