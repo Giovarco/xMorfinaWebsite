4d1c0f6ed70c4b7079329eacd8a1d91e30a646e4
Unknown <kirby94@hotmail.it>
2018-06-25 18:58:20 +0200

    - Home page: filter by tag draft 1

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -14,7 +14,10 @@ import { environment } from "../../environments/environment";
 
 export class HomePageComponent implements OnInit {
 
-  drawings: Drawing[];
+  drawings: Drawing[]; // All drawings
+  filteredDrawings: Drawing[]; // All drawings filtered by tag
+  useFilteredDrawings = false;
+  
   selectedDrawing: Drawing;
 
   facebookPageURL: string = environment.facebookPageURL;
@@ -36,6 +39,36 @@ export class HomePageComponent implements OnInit {
   openModal(content: any, drawing: Drawing) {
     this.selectedDrawing = drawing;
     this.modalService.open(content);
+  }
+
+  /**
+   * This function is called every time the user types on the tag INPUT
+   */
+  handleInputTags(event: any) {
+
+    const inputValue: string = event.target.value;
+
+    if ( inputValue === "" ) {
+      this.useFilteredDrawings = false;
+    } else {
+      this.filteredDrawings = [
+        {
+            "file" : "DarthVader.jpg",
+            "title" : "Darth Cupid",
+            "tags" : ["tag1", "tag2", "tag3"],
+            "date" : "2018-01-01"
+        },
+    
+        {
+            "file" : "Jack.jpg",
+            "title" : "My name is Jack",
+            "tags" : ["tag1", "tag2", "tag3"],
+            "date" : "2018-01-04"
+        }
+      ];
+      this.useFilteredDrawings = true;
+    }
+    
   }
 
 }
