22c1410d95cf80c6627b5c0c9228112b8611bb14
Unknown <kirby94@hotmail.it>
2018-06-26 06:04:15 +0200

    - Home page: filter by tag draft 4

--- src/services/drawings-by-tag-filter/drawings-by-tag-filter.service.ts
+++ src/services/drawings-by-tag-filter/drawings-by-tag-filter.service.ts
@@ -8,42 +8,84 @@ export class DrawingsByTagFilterService {
 
   constructor() { }
 
+  /**
+   * This function return a list of drawings that matches the user input tags.
+   * A drawing will be in this list if at least one of the user input tags matches partially ( = is substring of )
+   * at least one of the drawings tags
+   */
   static filterByTag(drawings: Drawing[], inputFilter: string): Drawing[] {
 
-    /**
-     * Input user tags
-     */
-    const tags: Array<string> = this.isValidFilter(inputFilter);
+    const validTags: Array<string> = this.isValidFilter(inputFilter);
 
-    if (tags) {
-      // Iterate over all drawings
-      return drawings.filter( (drawing: Drawing) => {
-
-        // Check if the current drawing has at least one tag that matches the inputFilter
-        let isCoherentDrawing = false;
-
-        // Iterate over all drawing tags
-        drawing.tags.forEach(singleDrawingTag => {
-          
-          // Iterate over all input user tags
-          tags.forEach(inputUserTag => {
-
-            // Check if the an input user tag is a substring of a single drawing tag 
-            if ( singleDrawingTag.indexOf(inputUserTag) !== -1 ) {
-              isCoherentDrawing = true;
-            }
-            
-          });
-
-
-        });
-
-        return isCoherentDrawing;
-
-      });
+    if (validTags) {
+      return DrawingsByTagFilterService.getCoherentDrawings(drawings, validTags);
     } else {
       return [] as Drawing[];
     }
+
+  }
+
+  /**
+   * This function return a list of drawings that match the user input tags.
+   * Please note that userTags is a safe source of information because it was preprocessed
+   */
+  private static getCoherentDrawings(drawings: Drawing[], userTags: Array<string>): Drawing[] {
+    
+    const coherentDrawings = drawings.filter( (drawing: Drawing) => {
+
+      /**
+       * This variable is true when the current drawing matches user input tags
+       */
+      let isDrawingCoherent = false;
+
+      /**
+       * This variable specifies the maximum number of matches between drawing tags and user tags
+       */
+      const maxMatches = drawing.tags.length  * userTags.length;
+
+      /**
+       * This variable specifies the number of drawing tags that match user input tags
+       */
+      let userTagMatches = 0;
+      
+      // Iterate over all drawing tags
+      drawing.tags.forEach( singleDrawingTag => {
+        if ( DrawingsByTagFilterService.doesDrawingTagsMatchUserTags(singleDrawingTag, userTags) ) {
+          userTagMatches++;
+        }
+      });
+
+      // Check if at least one user input tag match the drawings tags
+      if (userTagMatches > 0 && userTagMatches <= maxMatches) {
+        isDrawingCoherent = true;
+      }
+
+      return isDrawingCoherent;
+
+    });
+
+    // Return only coherent drawings
+    return coherentDrawings;
+
+  }
+
+  /**
+   * This functions returns true if singleDrawingTag is substring of at least one of the user tags
+   */
+  private static doesDrawingTagsMatchUserTags(singleDrawingTag: string, userTags: Array<string>) {
+
+    /**
+     * Match is true if singleDrawingTag is substring of at least one of the user tags
+     */
+    const match = userTags.some(singleUserTag => {
+      if ( singleDrawingTag.includes(singleUserTag) ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    return match;
 
   }
 
