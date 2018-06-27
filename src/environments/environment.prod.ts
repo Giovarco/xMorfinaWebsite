4c2b1d9ee23dbeae598975744662ca01b4def921
Unknown <kirby94@hotmail.it>
2018-06-27 15:16:14 +0200

    Home Page: get drawings information by REST call - N.B.: The actual code works, but unit tests do not. Whatever... :(

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  facebookPageURL: "https://www.facebook.com/XMorfinas-World-1553283851576465/"
+  facebookPageURL: "https://www.facebook.com/XMorfinas-World-1553283851576465/",
+  keys : {
+    "restDB" : "32700fa42519f5fd2f41354f36854935a61b6"
+  }
 };
